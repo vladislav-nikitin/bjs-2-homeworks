3432b5ee831b64ea4a2a11ab270a53b11d0358b5
Vladislav Nikitin <vlad.nikitin_1991@mail.ru>
2021-11-04 12:41:06 +0300

    my version

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,13 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
-  return result; // boolean
+  if (arr1.length === arr2.length)
+    return arr1.every((item, index) => item === arr2[index]);
+  else return false;
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
+  let resultArr = arr
+    .filter((number) => number % 3 === 0 && number > 0)
+    .map((number) => number * 10);
 
   return resultArr; // array
 }
