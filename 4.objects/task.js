ff57ecde36eddae59f8f71ae75fcac92f56ad1d1
Vladislav Nikitin <vlad.nikitin_1991@mail.ru>
2021-11-15 16:05:03 +0300

    my solution

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,55 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
-}
+  this.subject = subjectName;
+};
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = [mark];
+  } else {
+    this.marks.push(mark);
+  }
+};
+
+Student.prototype.addMarks = function (...marks) {
+  if (this.marks === undefined) {
+    this.marks = [...marks];
+  } else {
+    this.marks.push(...marks);
+  }
+};
+
+Student.prototype.getAverage = function () {
+  let sum = 0;
+  let count = 0;
+  for (let i = 0; i < this.marks.length; i++) {
+    count += 1;
+    sum += this.marks[i];
+  }
+  console.log(sum / count);
+};
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+};
+
+let student1 = new Student("Tony", "male", 37);
+student1.setSubject("Algebra");
+// student1.addMark(5);
+// student1.addMark(4);
+// student1.addMark(5);
+student1.addMarks(5, 4, 3, 4, 5);
+student1.getAverage();
+student1.exclude("low grades");
+console.log(student1);
+
+let student2 = new Student("Buzz", "female", 35);
+// student2.setSubject("Geometry");
+// console.log(student2);
