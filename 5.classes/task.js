44264650a0bb3e8ede7024e744d28248ae17edc1
Vladislav Nikitin <vlad.nikitin_1991@mail.ru>
2021-11-24 18:07:31 +0300

    my new version

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -105,17 +105,20 @@ class Library {
     for (let i = 0; i <= this.books.length; i++) {
       if (this.books[i][type] === value) {
         return this.books[i];
-      } else return null;
+      }
     }
+    return null;
   }
 
   giveBookByName(bookName) {
     for (let i = 0; i <= this.books.length; i++) {
       if (this.books[i].name === bookName) {
-        this.books.splice(this.books[i]);
-        return this.books[i];
-      } else return null;
+        let founded = this.books[i];
+        this.books.splice(founded, 1);
+        return founded;
+      }
     }
+    return null;
   }
 }
 
